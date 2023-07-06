f58a2649b2be93636c24d21df8fdb64ebbdba1ad
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-06 17:59:14 +0300

    Только уникальные элементы в начало

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,16 +12,8 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            ListNode list13 = new ListNode(4);
-            ListNode list12 = new ListNode(2, list13);
-            ListNode list1 = new ListNode(1, list12);
-
-            ListNode list23 = new ListNode(4);
-            ListNode list22 = new ListNode(3, list13);
-            ListNode list2 = new ListNode(1, list12);
-
-
-            Console.WriteLine(MergeTwoSortedLists.MergeTwoLists(list1, list2));
+            int[] nums = { 1, 1, 2 };
+            Console.WriteLine(RemoveDuplicatesFromSortedArray.RemoveDuplicates(nums));
         }
     }
 }
