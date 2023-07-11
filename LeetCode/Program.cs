3ecf6d955552cef8e33ce4c8a9f3e93b0a0b3a60
Максим Ярочевский <maximyarochevskygithub@gmail.com>
2023-07-11 19:46:37 +0300

    Бинарный поиск

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -11,66 +11,39 @@ namespace LeetCode
     
     public class Program
     {
-        public static void ArrayToConsole<T>(T [] array)
+        public static void WriteArrayToConsole<T>(T [] array)
         {
             foreach (var value in array)
             {
                 Console.Write($"{value} ");
             }
         }
-        static void Main(string[] args)
+
+        public static int BinarySearch(int[] array, int value, int start, int end)
         {
-            int[] nums = { -1, 1, 0, -3, 3 };
-            int[] answer = ProductExceptSelf(nums);
-            ArrayToConsole(answer);
-        }
-        
-        public static int[] ProductExceptSelf(int[] nums)
-        {
-            int[] answer = new int[nums.Length];
+            int center = (start+end) / 2;
 
-            int multiplication = 1;
+            if (array[center] == value)
+                return center;
 
-            List<int> zeros = new List<int>();
-
-            for (int i = 0; i < nums.Length; i++)
+            else if (array[center] > value)
             {
-                if (nums[i] == 0)
-                {
-                    zeros.Add(i);
-                    continue;
-                }
-                multiplication *= nums[i];
-            }
-
-            if (zeros.Count > 1)
-            {
-                //для заполнения массива одним числов используем цикл for, т.к он в 4 раза быстрее чем Enumerable.Repeat()
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    answer[i] = 0;
-                }
-
-            }
-            else if (zeros.Count() == 1)
-            {
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    if (i == zeros.First())
-                        answer[i] = multiplication - nums[i];
-                    else
-                        answer[i] = 0;
-                }
+                start = 0;
+                end = center;
+                return BinarySearch(array, value, start, end);
             }
             else
             {
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    answer[i] = multiplication - nums[i];
-                }
+                start = center;
+                end = array.Length;
+                return BinarySearch(array, value, start, end);
             }
+        }
 
-            return answer;
+        static void Main(string[] args)
+        {
+            int[] array = { 1,2,3,4,5,6,7,8};
+            Console.WriteLine(BinarySearch(array,6,0,array.Length));
         }
     }
 }
